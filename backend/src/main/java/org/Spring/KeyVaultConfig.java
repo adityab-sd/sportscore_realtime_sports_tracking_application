f5b4b5c493de6713b330f26434947dbe630a2b83
mahanthvamsi <mahanthvamsis1@gmail.com>
2026-07-10 13:42:48 +0100

    FEAT075 - Navbar mobile responsive

--- backend/src/main/java/org/Spring/KeyVaultConfig.java
+++ backend/src/main/java/org/Spring/KeyVaultConfig.java
@@ -17,25 +17,25 @@ public class KeyVaultConfig {
     @Value("${azure.keyvault.uri}")
     private String keyVaultUri;
 
-    @Bean
-    public SecretClient secretClient() {
-        return new SecretClientBuilder()
-                .vaultUrl(keyVaultUri)
-                .credential(new DefaultAzureCredentialBuilder().build())
-                .buildClient();
-    }
+    // @Bean
+    // public SecretClient secretClient() {
+    //     return new SecretClientBuilder()
+    //             .vaultUrl(keyVaultUri)
+    //             .credential(new DefaultAzureCredentialBuilder().build())
+    //             .buildClient();
+    // }
 
-    @Bean
-    public MapPropertySource keyVaultPropertySource(SecretClient secretClient,
-                                                     ConfigurableEnvironment environment) {
-        Map<String, Object> secrets = Map.of(
-                "eventhub.connection-string", secretClient.getSecret("eventhub-connection-string").getValue(),
-                "eventhub.name",              secretClient.getSecret("eventhub-name").getValue(),
-                "apisports.key",              secretClient.getSecret("apisports-key").getValue()
-        );
+    // @Bean
+    // public MapPropertySource keyVaultPropertySource(SecretClient secretClient,
+    //                                                  ConfigurableEnvironment environment) {
+    //     Map<String, Object> secrets = Map.of(
+    //             "eventhub.connection-string", secretClient.getSecret("eventhub-connection-string").getValue(),
+    //             "eventhub.name",              secretClient.getSecret("eventhub-name").getValue(),
+    //             "apisports.key",              secretClient.getSecret("apisports-key").getValue()
+    //     );
 
-        MapPropertySource propertySource = new MapPropertySource("keyVaultSecrets", secrets);
-        environment.getPropertySources().addFirst(propertySource);
-        return propertySource;
-    }
+    //     MapPropertySource propertySource = new MapPropertySource("keyVaultSecrets", secrets);
+    //     environment.getPropertySources().addFirst(propertySource);
+    //     return propertySource;
+    // }
 }
